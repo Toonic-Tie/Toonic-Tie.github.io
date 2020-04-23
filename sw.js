74cb56df5daab56965c5984bae1da2c0610c3774
Toonic-Tie <55710122+Toonic-Tie@users.noreply.github.com>
2020-04-23 13:19:15 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,13 +1,5 @@
-var CACHE_STATIC_NAME = 'static-v3';
-var CACHE_DYNAMIC_NAME = 'dynamic-v3';
-
-var deferredPrompt;
-self.addEventListener('beforeinstallprompt', function(event) {
-  console.log('beforeinstallprompt fired');
-  event.preventDefault();
-  deferredPrompt = event;
-  return false;
-});
+var CACHE_STATIC_NAME = 'static-v1';
+var CACHE_DYNAMIC_NAME = 'dynamic-v1';
 
 self.addEventListener('install', function(event) {
   console.log('[Service Worker] Installing Service Worker ...', event);
